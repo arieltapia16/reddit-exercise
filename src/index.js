87e45aa2c50bc74fccff994c7a57d9c95b1711db
Ariel Tapia <ariel.tapia@boats.com>
2020-04-05 17:38:17 -0300

    Added Readme

--- src/index.js
+++ src/index.js
@@ -5,11 +5,12 @@ import { Provider } from 'react-redux'
 import configureStore from './store';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import * as serviceWorker from './serviceWorker';
+
 ReactDOM.render(
   <Provider store={configureStore()}>
     <App />
   </Provider>,
   document.getElementById('root')
 );
-registerServiceWorker();
+serviceWorker.unregister();
